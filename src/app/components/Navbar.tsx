0ea4b8fc65a50034d6259c4f88484618f0b6f179
Sharadindu Das <remodas7774@gmail.com>
2024-05-17 13:21:39 +0530

    changes to the UI for navbar done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,25 +1,33 @@
 import Link from "next/link"
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
 
 const Navbar = () => {
     return (
         <header className="h-20 flex items-center fixed left-0 right-0 w-full top-0 bg-white shadow-md z-10">
-            <div className="container mx-auto flex justify-between">
+            <div className="container mx-auto flex justify-between items-center">
                 <div>
                     <Link href="/" className="w-16 font-black text-xl uppercase">
                         <span>Swift</span>
                         <span className="text-purple-600">Buy</span>
                     </Link>
                 </div>
-                <div className="text-base font-medium flex items-center gap-x-8">
-                    <Link href="/">Home</Link>
-                    <Link href="/">About</Link>
-                    <Link href="/checkout">
-                        Cart <span>(0)</span>
-                    </Link>
-                </div>
-                <div className="text-base font-medium text-white">
-                    <Link href="/login" className="bg-purple-600 px-6 py-3 rounded-lg hover:bg-purple-700 transition-colors">Login</Link>
-                    <Link href="/signup" className="bg-purple-600 px-6 py-3 ml-4 rounded-lg hover:bg-purple-700 transition-colors">Signup</Link>
+                <div className="flex items-center gap-x-10">
+                    <div className="text-base font-medium flex items-center gap-x-8 mr-5">
+                        <Link href="/">Home</Link>
+                        <Link href="/">Shop</Link>
+                        <Link href="/">Blog</Link>
+                        <Link href="/">Contact</Link>
+                    </div>
+                    <div className="text-base flex items-center gap-x-4">
+                        <Link href="/signup">
+                            <FaRegUserCircle className="text-2xl" />
+                        </Link>
+                        <Link href="/checkout" className="relative">
+                            <span className="absolute -top-3.5 -translate-x-1/2 left-1/2 text-xs bg-purple-400 text-white w-5 h-5 flex items-center justify-center rounded-full">0</span>
+                            <IoCartOutline className="text-3xl" />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </header>
