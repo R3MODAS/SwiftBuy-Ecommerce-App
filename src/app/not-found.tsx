f7286375dd82cf24b460fd6c8d30d75330d3d78e
Sharadindu Das <remodas7774@gmail.com>
2024-05-18 18:41:59 +0530

    fix of apos in error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <div className="container mx-auto">
         <h2 className='text-8xl font-bold'>404</h2>
         <div className="error-bg"></div>
-        <h3 className='text-3xl font-semibold'>Looks like you're lost</h3>
+        <h3 className='text-3xl font-semibold'>Looks like you&apos;re lost</h3>
         <p className='text-base font-medium'>the page you are looking for is not available!</p>
         <Link href="/" className="bg-purple-600 text-white px-5 py-[10px] my-5 inline-block rounded-md">Go to Home</Link>
       </div>
